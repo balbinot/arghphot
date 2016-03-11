b2149ca98508b2599acdb0f1f116d30fac4da973
Eduardo Balbinot <eduardo.balbinot@gmail.com>
2016-03-11 18:32:01 +0000

    added PSF selection tool, testing

--- arghphot/arghphot.py
+++ arghphot/arghphot.py
@@ -6,9 +6,11 @@
 from matplotlib import pyplot as p
 
 import numpy as np
-from astropy.io import fits
+from astropy.io import fits, ascii
+from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy import wcs
+import aplpy
 from pyraf import iraf
 
 # Logger
@@ -49,6 +51,7 @@ class Frame(mylogger):
         ## Utility names
         self.daofindfn = '%s%d.coo.1' % (self.pfname, ext)
         self.photfn = '%s%d.mag.1' % (self.pfname, ext)
+        self.pstfile = '%s%d.pst.1' % (self.pfname, ext)
         self.fitpsffn = self.pfname.replace('.fits', '.fitpsf')
 
     def pix2sky(self, x, y):
@@ -111,6 +114,16 @@ class Frame(mylogger):
         iraf.fitskypars.setParam('dannulus',2.*self.fwhm)
         iraf.phot(mode='h',Stdout=1)
 
+    def trim_phot(self):
+        a = ascii.read(self.photfn)
+        std = a['STDEV']
+        sum = a['SUM']/a['AREA']
+        sky = a['MSKY']
+        sn = np.abs(sum-sky)/std
+        i = sn > 3
+        tmp = a[i]
+        tmp.write(self.photfn+'trim', format='ascii')
+
     def run_fitpsf(self):
 
         # select some guess stars for PSF building
@@ -126,11 +139,10 @@ class Frame(mylogger):
         iraf.fitpsf(mode='h',Stdout=1)
 
     def merge(self):
-        iraf.txdump(textfile=self.photfn,
-                    fields='xcenter,ycenter,mag,msky,merr,id',
-                    expr='mag!=INDEF && merr!=INDEF',
-                    Stdout=self.base+'phottmp')
-        mags = np.loadtxt(self.base+'phottmp')
+        ## Use trimmed photometry to avoid variable sky background spurious
+        ## detections
+        # x,y, msky, stdev, sum, area, mag, merr, id
+        mags = np.loadtxt(self.photfn+'trim', usecols=(6,7,14,15,26,27,29,30,3), skiprows=1)
 
         iraf.txdump(textfile=self.daofindfn,
                     fields='sharpness,sround,ground,id',
@@ -144,33 +156,106 @@ class Frame(mylogger):
                     Stdout=self.base+'fitpsftmp')
         fitpsf = np.loadtxt(self.base+'fitpsftmp')
 
-
+        ## I have no idea how this works
         I = reduce(lambda l,r: np.intersect1d(l,r,True), (i[:,-1] for i in
-                                                          (daofind, mags,
+                                                          (mags, daofind,
                                                            fitpsf)))
-
-
-        oo = np.c_[daofind[np.searchsorted(daofind[:,-1], I)],
-                  mags[np.searchsorted(mags[:,-1], I)],
-                  fitpsf[np.searchsorted(fitpsf[:,-1], I)]]
+        oo = np.c_[mags[np.searchsorted(mags[:,-1], I)],
+                   daofind[np.searchsorted(daofind[:,-1], I)],
+                   fitpsf[np.searchsorted(fitpsf[:,-1], I)]]
 
         return oo
+
+    def select_psf(self):
+        f = self.merge()
+        w = self.pix2sky(f[:,0], f[:,1])
+        coo = SkyCoord(w[:,0]*u.deg, w[:,1]*u.deg)
+        nid, nsep, _ = coo.match_to_catalog_sky(coo, nthneighbor=2)
+        x = f[:,0]
+        y = f[:,1]
+        id = f[:,-1]
+        sky = f[:,2]
+        skystd = f[:,3]
+        mag = f[:,6]
+        merr = f[:,7]
+        sharp = f[:,9]
+        fwhm = f[:,13]
+
+        ## Set of constrains for PSF stars
+        i = (mag < np.mean(mag) - 1.2*np.std(mag))
+        i *= (merr < 0.02)
+        i *= (np.abs(sharp-np.median(sharp)) < 0.04)
+        i *= (fwhm < 1.05*tmp.fwhm/2.355)
+        i *= (fwhm > 0.8*tmp.fwhm/2.355)
+        i *= (nsep.arcsec > 15*tmp.fwhmph)
+        i *= (x > 60*tmp.fwhm)*(y > 60*tmp.fwhm)
+        i *= (x < tmp.hdu.data.shape[1] - 60*tmp.fwhm)
+        i *= (y < tmp.hdu.data.shape[0] - 60*tmp.fwhm)
+        i *= (np.abs(skystd - np.mean(skystd)) < np.std(skystd))
+        i *= (sky - np.mean(sky) < np.std(sky))
+
+        self._parse_pst(id[i], x[i], y[i], mag[i], sky[i])
+        return (id[i], x[i], y[i], mag[i], sky[i])
+
+    def tvmark(self, ra, dec):
+        gc = aplpy.FITSFigure(self.hdu)
+        gc.show_grayscale(stretch='arcsinh')
+        gc.set_tick_labels_font(size='small')
+        gc.show_markers(ra,dec,layer='scatter',edgecolor='red',
+                        facecolor='none',marker='o',s=10,alpha=0.5)
+
+    def _parse_pst(self, id, x, y, mag, msky):
+        pstfile = open(self.pstfile, 'w')
+        pstfile.write("#N ID    XCENTER   YCENTER   MAG         MSKY                                  \\\n")
+        pstfile.write("#U ##    pixels    pixels    magnitudes  counts                                \\\n")
+        pstfile.write("#F %-9d  %-10.3f   %-10.3f   %-12.3f     %-15.7g                                 \n")
+        pstfile.write("#\n")
+        np.savetxt(pstfile, np.array([id, x,y, mag, msky]).T,
+                   fmt=['%-9d','%-10.3f','%-10.3f','%-12.3f','%-15.7g'])
+        pstfile.close()
+
+    def grid_psf(self):
+        from mpl_toolkits.axes_grid1 import ImageGrid
+        id, x, y = np.loadtxt(self.pstfile, usecols=(0,1,2), unpack=True)
+
+
+        grid1 = ImageGrid(fig, 111,
+                nrows_ncols = (2, 4),
+                axes_pad = 0.07,
+                share_all=True,
+                label_mode = "L",
+                cbar_location = "right",
+                cbar_mode="single",
+                cbar_size="7%",
+                cbar_pad="7%",
+                aspect = True
+                )
+
+
+        xbox = int(f[i,0] - rad)
+        Xbox = int(f[i,0] + rad)
+        ybox = int(f[i,1] - rad)
+        Ybox = int(f[i,1] + rad)
+        block = img[xbox:Xbox,ybox:Ybox]
+
+
 
 
 
 if __name__=='__main__':
     fname = "/scratch/gc_survey/raw_data/c4d_150715_013102_osi_g_v1.fits"
     tmp = Frame(fname, 2, 'bunda.log')
-    t = tmp.pix2sky(np.random.rand(10)*1000, np.random.rand(10)*1000)
     tmp.findsky(10, 1000)
     #tmp.run_daofind()
     #tmp.run_phot()
+    tmp.trim_phot()
     #tmp.run_fitpsf()
 
-    f = tmp.merge()
-    print f[0]
+    f = tmp.select_psf()
 
-    p.plot(f[:,0], f[:,1], 'k.', ms=1)
+    t = tmp.pix2sky(f[1], f[2])
+    tmp.tvmark(t[:,0], t[:,1])
+
     p.show()
 
 
